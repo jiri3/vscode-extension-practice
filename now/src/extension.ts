7499e18d0d6699d7e6a87d04495d7bdf4a0e4931
jiri3 <kotsukotsu@kotsukotsu.work>
2020-10-02 02:16:38 +0900

    [update]日付のフォーマットをユーザが選択できるようにする

--- now/src/extension.ts
+++ now/src/extension.ts
@@ -2,21 +2,40 @@ import * as vscode from "vscode";
 import * as moment from "moment";
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log(
-    'Congratulations, your extension "extension-practice" is now active!'
-  );
+  // showQuickPickにて表示するフォーマットのリストを生成する
+  const formatList = Object.values(moment.HTML5_FMT)
+    .sort()
+    .reverse()
+    .map<vscode.QuickPickItem>((value) => {
+      const example = moment("2000-01-23T12:34:56.789").format(value);
+      return {
+        label: value,
+        description: `e.g. ${example}`,
+      };
+    });
 
-  let disposable = vscode.commands.registerTextEditorCommand(
+  let disposable = vscode.commands.registerCommand(
     "extension-practice.now",
-    (texitEditor, edit) => {
-      texitEditor.selections.forEach((select) => {
-        const replaceRange = new vscode.Range(select.start, select.end);
-        const now = moment();
-        edit.replace(
-          replaceRange,
-          now.format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS)
-        );
+    async () => {
+      // 挿入する日付のフォーマットをユーザに選択してもらう
+      const value = await vscode.window.showQuickPick(formatList, {
+        placeHolder: "please select format",
       });
+
+      const textEditor = vscode.window.activeTextEditor;
+      if (!textEditor || !value) {
+        return;
+      }
+
+      // 日付を挿入する
+      const now = moment();
+      const insertValue = now.format(value.label);
+      for (const select of textEditor.selections) {
+        await textEditor.edit((builder) => {
+          const replaceRange = new vscode.Range(select.start, select.end);
+          builder.replace(replaceRange, insertValue);
+        });
+      }
     }
   );
 
